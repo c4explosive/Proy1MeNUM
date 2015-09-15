230d75ae9b4c06986d1cbe25917a189a12837ee7
c4explosive <ao946391@gmail.com>
2015-09-14 23:23:56 -0500

    rel_Abs_menu

--- taylor.c
+++ taylor.c
@@ -11,6 +11,8 @@
 double fact=1.0;
 double tayl=0;
 double fxx=0;
+double eabs=0;
+double erel=0;
 typedef struct _fxpol
 {
     double A;
@@ -21,7 +23,6 @@ typedef struct _fxpol
     double x0;
     int n;
 } fxpol;
-
 fxpol * derive_fx(fxpol * fxp, int veces)
 {
     if(veces==0)
@@ -35,11 +36,20 @@ fxpol * derive_fx(fxpol * fxp, int veces)
 	derive_fx(fxp,veces);
     }
 }
+
+void initz()
+{
+    fact=1.0;
+    tayl=0;
+    fxx=0;
+    eabs=0;
+    erel=0;    
+}
 double factorial(double x)
 {
-     if(x==0)
+    if(x==0)
 	return fact*1;
-     else
+    else
 	fact=x*factorial(x-1);
 }
 void imp_pol(fxpol * fx)
@@ -75,14 +85,6 @@ void input(fxpol * fx)
     }
 
 }
-/*double ipow(double b, double n)
-{
-    printf("N: %f\n",n);
-    if (n<0)
-	return (1/pow(b,-n));
-    else
-	return pow(b,n);
-}*/
 
 double ABSn(double err)
 {
@@ -100,7 +102,7 @@ double er_abs(double tayl)
 
 double er_rel(double tayl)
 {
-    double err=er_abs(tayl)/ABSn(fxx);
+    double err=2*(er_abs(tayl)/(ABSn(tayl)+ABSn(fxx)));
     return err;
 }
 
@@ -120,12 +122,14 @@ double taylors(fxpol * fx,double ic)
      double err_REL=er_rel(tayl);
      printf("EABS: %.30f\n",err_ABS);
      printf("EREL: %.30f\n",err_REL);
-     if(err_REL<=epsilon)
+     if(err_ABS<=epsilon)
      {
 	fact=1;
 	tayl+=((eval_fx(fx->A,fx->B,fx->C,fx->D,fx->x0))*pow((fx->x-fx->x0),ic))/factorial(ic);
         printf("IC: %.0f;\tTaylor:\t%.30f\n",ic,tayl);
 	ic=0;
+	eabs=err_ABS;
+	erel=err_REL;
 	return tayl;
      }
      else
@@ -154,14 +158,68 @@ void print_terms(fxpol * fx)
 //TODO: La derivada al evaluarla con 0 causa estragos, no se está haciendo la derivada real, se está simulando.
 //Parcialmente reparada la derivada.
 
+void ntayl()
+{
+    clrscr();
+    initz();
+    fxpol * fx1=initfx(fx1);
+    //fx1->A=1;fx1->B=3;fx1->C=1;fx1->D=4;fx1->x=2;fx1->x0=1;
+    printf("Ingrese los coeficientes para la función de tipo: Ax^B+Ce^(-Dx)\n");
+    input(fx1);
+    fxx=eval_fx(fx1->A,fx1->B,fx1->C,fx1->D,fx1->x);
+    printf("Resultado de la serie de Taylor: %.5f\n",taylors(fx1,0));
+    printf("Error Absoluto: %.5f\n",eabs);
+    printf("Error Relativo Porcentual: %.5f%\n",erel*100);
+    printf("Valor Exacto de la función: %.5f\n",fxx);
+    getch();
+}
+void presentacion()
+{
+	clrscr();
+	printf("\n                      Universidad Tecnol¢gica de Panama \n");
+	printf("            Facultad de Ingenier¡a en Sistemas Computacionales\n");
+	printf("           Departamento de Computaci¢n y Simulaci¢n de Sistemas\n\n");
+	printf("                         Métodos Numéricos para Ingenieros\n\n");
+	printf("                               Proyecto nø1\n");
+	printf("                        Ingeniera Jacqueline De Ching\n\n");
+	printf("                                   Grupo: \n");
+	printf("                          Espinosa, Angel  8-905-1352    \n"); 
+    printf("                             Miranda, Yaneys  8-879-525\n\n");
+	printf("                                   1IL-121\n");
+	getch();
+}
+
+void menup()
+{
+    int op, cont=1, lmp=1;
+    char * opp=malloc(sizeof(char));
+    do
+    {
+	if (lmp)
+            clrscr();
+	printf("\t\t\tMENU PRINCIPAL\n");
+	printf("1. Presentacion\n");
+	printf("2. Cálculo de la Serie de Taylor\n");
+	printf("3. Salir del Programa\n");
+	printf("Ingrese una opcion: ");
+	scanf("%s",opp);
+	op=atoi(opp);
+	switch (op)
+	{
+	    case 1: presentacion();		    
+		    lmp=1;
+		    break;
+	    case 2: ntayl(); break;
+	    case 3: cont=0; break;
+	    default: printf("Escriba una opcion correcta.\n"); break;
+	}
+    } while( cont );
+}
+
+
+
 int main()
 {
-    fxpol * fx1=initfx(fx1);
-    //int n=500;
-    fx1->A=1;fx1->B=3;fx1->C=1;fx1->D=4;fx1->x=2;fx1->x0=1;
-    //input(fx1);
-    fxx=eval_fx(fx1->A,fx1->B,fx1->C,fx1->D,fx1->x);
-    printf("fx: %.30f\n",fxx);
-    printf("Taylors Series: %.30f\n",taylors(fx1,0));
+    menup();
     return 0;
 }
